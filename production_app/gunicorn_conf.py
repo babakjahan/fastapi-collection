984e51137a83a0e03b2a1f6ed9b108fef847e3aa
Babak Jahangiri <babakjahangiri@gmail.com>
2025-07-12 19:51:59 +0100

    final update

--- production_app/gunicorn_conf.py
+++ production_app/gunicorn_conf.py
@@ -6,4 +6,4 @@ bind: str = "0.0.0.0:8080" #! change to your desired host and port
 workers: int = multiprocessing.cpu_count() * 2 + 1
 # worker_class: str = "uvicorn.workers.UvicornWorker" #! legacy import
 worker_class: str = "uvicorn_worker.UvicornH11Worker"
-keepalive = 24 * 60 * 60
+keepalive = 5
